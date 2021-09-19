ebc4cc3f984c61970d905956336480069ff2fd00
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-09-19 20:37:30 +0530

    LoginBrandInfo: refactor layout and use @brainhubeu/react-carousel
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/components/LoginBrandInfo/index.tsx
+++ src/components/LoginBrandInfo/index.tsx
@@ -1,26 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import { autoplayPlugin, Dots } from '@brainhubeu/react-carousel';
 
-import Image from 'components/common/Image';
-
-import logo from 'assets/icons/logo.svg';
-import dot from 'assets/icons/dot.svg';
-import elongatedDot from 'assets/icons/elongated-dot.svg';
+const Carousel = dynamic(() => import('@brainhubeu/react-carousel'), {
+	ssr: false,
+});
 
 import styles from './styles.module.scss';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 const LoginBrandInfo = () => {
-	const [currValue, setCurrValue] = useState(0);
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (currValue === 2) setCurrValue(0);
-			else setCurrValue(currValue + 1);
-		}, 5000);
-		return () => {
-			clearInterval(interval);
-		};
-	});
-
+	const [value, setValue] = useState(0);
 	const texts = [
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur ipsum quis gravida laoreet. Vivamus aliquet dui eu mollis dictum. Curabitur est magna, volutpat scelerisque consectetur sed, faucibus eget tellus. Donec ut nisl eu justo aliquam interdum. Vestibulum ornare posuere odio vel faucibus. Sed mi tellus, lobortis sed viverra sed, pharetra sed orci. Donec ultricies enim non quam pretium porta.',
 		'Sed ac elementum nisi. Nullam convallis nibh turpis, id iaculis enim bibendum id. Aliquam a dignissim ex. Nulla quis accumsan enim, sed efficitur odio. Praesent tellus arcu, maximus ut malesuada et, sollicitudin euismod metus. Mauris semper id magna in sodales. Donec gravida viverra ligula, quis euismod erat fermentum tempus. Donec lacus nunc, lobortis non purus non, elementum hendrerit nisl. Vestibulum .',
@@ -29,20 +19,31 @@ const LoginBrandInfo = () => {
 
 	return (
 		<div className={styles.infoContainer}>
-			<div className={styles.contentWrapper}>
-				<Image className={styles.logo} src={logo} alt='' />
-
-				<p>{texts[currValue]}</p>
-				<div className={styles.navigationDots}>
-					{texts.map((e, index) => (
-						<Image
-							key={e}
-							src={index === currValue ? elongatedDot : dot}
-							onClick={() => setCurrValue(index)}
-							alt='dot'
-						/>
-					))}
-				</div>
+			<div className={styles.carouselContainer}>
+				<Carousel
+					value={value}
+					onChange={val => setValue(val)}
+					plugins={[
+						'infinite',
+						{
+							resolve: autoplayPlugin,
+							options: {
+								interval: 6000,
+								stopAutoPlayOnHover: true,
+							},
+						},
+					]}
+					draggable={false}>
+					<p>{texts[0]}</p>
+					<p>{texts[1]}</p>
+					<p>{texts[2]}</p>
+				</Carousel>
+				<Dots
+					value={value}
+					number={3}
+					onChange={val => setValue(val)}
+					className={styles.dot}
+				/>
 			</div>
 		</div>
 	);
