424aa0d09cbb18fb664b85d440c703c6bab13c7d
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-09-25 17:05:39 +0530

    src: add progress bar to show when navigating different pages
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import useProgressBar from 'hooks/useProgressBar';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	useProgressBar();
+
+	return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
