5e7e32a92e48ab1b8d7fbea07db9d080b14898da
Kartik Bhalla <kartikbhalla12@gmail.com>
2021-09-30 14:56:25 +0530

    LoginBrandInfo: fix styles and cleanup
    Signed-off-by: Kartik Bhalla <kartikbhalla12@gmail.com>

--- src/components/auth/LoginBrandInfo/index.tsx
+++ src/components/auth/LoginBrandInfo/index.tsx
@@ -9,7 +9,7 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 const LoginBrandInfo = () => {
 	const texts = [
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur ipsum quis gravida laoreet. Vivamus aliquet dui eu mollis dictum. Curabitur est magna, volutpat scelerisque consectetur sed, faucibus eget tellus. Donec ut nisl eu justo aliquam interdum. Vestibulum ornare posuere odio vel faucibus. Sed mi tellus, lobortis sed viverra sed, pharetra sed orci. Donec ultricies enim non quam pretium porta.',
-		'Sed ac elementum nisi. Nullam convallis nibh turpis, id iaculis enim bibendum id. Aliquam a dignissim ex. Nulla quis accumsan enim, sed efficitur odio. Praesent tellus arcu, maximus ut malesuada et, sollicitudin euismod metus. Mauris semper id magna in sodales. Donec gravida viverra ligula, quis euismod erat fermentum tempus. Donec lacus nunc, lobortis non purus non, elementum hendrerit nisl. Vestibulum .',
+		'Sed ac elementum nisi. Nullam convallis nibh turpis, id iaculis enim bibendum id. Aliquam a dignissim ex. Nulla quis accumsan enim, sed efficitur odio. Praesent tellus arcu, maximus ut malesuada et, sollicitudin euismod metus. Mauris semper id magna in sodales. Donec gravida viverra ligula, quis euismod erat fermentum tempus. Donec lacus nunc, lobortis non purus non, elementum hendrerit nisl. Vestibulum.',
 		'Integer aliquam neque odio, vitae semper erat ornare non. Donec ut dictum metus. Nullam condimentum sed tortor sed imperdiet. Quisque rutrum, eros id condimentum tincidunt, turpis turpis suscipit sapien, a elementum tortor tellus id quam. Nunc mollis hendrerit lorem, eu lobortis nisl gravida molestie. Sed rhoncus ac odio in eleifend. Fusce ac mattis nunc. Sed varius fringilla nisi, quis mollis erat.',
 	];
 
@@ -18,15 +18,15 @@ const LoginBrandInfo = () => {
 			<div className={styles.carouselContainer}>
 				<Logo className={styles.logo} />
 				<Carousel
-					showThumbs={false}
-					className={styles.carousel}
-					infiniteLoop
-					showStatus={false}
-					showArrows={false}
 					autoPlay
+					className={styles.carousel}
 					emulateTouch
-					swipeable
+					infiniteLoop
 					interval={5000}
+					showArrows={false}
+					showStatus={false}
+					showThumbs={false}
+					swipeable
 					renderIndicator={(
 						clickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void,
 						isSelected: boolean
@@ -35,9 +35,9 @@ const LoginBrandInfo = () => {
 							className={`${styles.dot} ${isSelected ? styles.selected : ''}`}
 							onClick={clickHandler}></div>
 					)}>
-					{texts.map(t => (
-						<div key={t}>
-							<p>{t}</p>
+					{texts.map((text, key) => (
+						<div key={key} className={styles.itemContainer}>
+							<p className={styles.itemText}>{text}</p>
 						</div>
 					))}
 				</Carousel>
